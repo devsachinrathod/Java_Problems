aa4a6da1b03772cd53a47bc37279a0408caac600
Sachin Rathod <sr5631080@gmail.com>
2025-09-28 23:12:57 +0530

    binary -search example

--- Roomates.java
+++ Roomates.java
@@ -1,19 +1,33 @@
 public class Roomates {
 
-    private String name;
-    private String address;
+    // Binary Search method
+    static int bs(int[] nums, int k) {
+        int low = 0, hi = nums.length - 1;
 
-    Roomates(String name, String address) {
-        this.name = name;
-        this.address = address;
-    }
-
-    void display() {
-        System.out.println("The name of roommate: " + name + " and address: " + address);
+        while (low <= hi) {
+            System.out.println("Current range is : " + low + hi);
+            int mid = (low + hi) / 2;
+            if (nums[mid] == k) {
+                return mid; // found
+            } else if (nums[mid] > k) {
+                hi = mid - 1; // search left
+            } else {
+                low = mid + 1; // search right
+            }
+        }
+        return -1; // not found
     }
 
     public static void main(String[] args) {
-        Roomates n1 = new Roomates("sachin", "Nanded, Maharastra");
-        n1.display();
+        int[] arr = {2, 5, 8, 12, 16, 23, 38};
+        int target = 38;
+
+        int result = bs(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array");
+        }
     }
 }
