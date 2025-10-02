ccc720b9f4c16495a7db5dfdf0d3ef84f57d8ef4
Sachin Rathod <sr5631080@gmail.com>
2025-10-02 14:34:47 +0530

    Nested class and inherits

--- outerClass.java
+++ outerClass.java
@@ -1,19 +1,34 @@
-class outerClass{
-  
-    void show(){
+
+class outerClass {
+
+    outerClass() {
+        System.out.println("Outer class constructor called");
+    }
+
+    void show() {
         System.out.println("This is the outer class");
     }
 
-    class NestedInnerClass{
+    class NestedInnerClass extends  outerClass{
+
         void display(){
+            super.show();
             System.out.println("This is a nested inner class");
+        }
+
+        class DeepNestedClass {
+            void print() {
+                System.out.println("This is a deep nested class");
+            }
         }
     }
 
-    public static void main(String[] args){
-      outerClass outer = new outerClass();
-      outer.show();
-      outerClass.NestedInnerClass inner = outer.new NestedInnerClass();
-      inner.display();
+    public static void main(String[] args) {
+        outerClass outer = new outerClass();
+        outer.show();
+        outerClass.NestedInnerClass inner = outer.new NestedInnerClass();
+        inner.display();
+        outerClass.NestedInnerClass.DeepNestedClass deep = inner.new DeepNestedClass();
+        deep.print();
     }
 }
