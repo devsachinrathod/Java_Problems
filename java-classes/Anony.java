9bbbcb40acb76159f41f0208b0b2e4168ffb8ad3
Sachin Rathod <sr5631080@gmail.com>
2025-10-03 23:16:21 +0530

    new code

--- java-classes/Anony.java
+++ java-classes/Anony.java
@@ -68,31 +68,55 @@
 
 interface Info {
     void display();
+    void extraMethod();
 }
 
  class Anony implements Info {
  
- public String name;
- public int age;
+ public String name = "Default Name";
 
+ public int age = 21;
+
+ public void printTown(){
+    System.out.println("This is a method in Anony class");
+ }
+
+@Override
+public void extraMethod(){
+    System.out.println("This is an extra method in Anony class");
+}
+@Override
  public void display(){
     System.out.println("Name: "+name);
     System.out.println("Age: "+age);
  }
  public static void main(String[] args){
+
     Anony obj = new Anony(){
         {
             name = "John";
             age = 25;
         }
+        
         @Override
         public void display(){
+            System.out.println(super.name);
             super.display();
             System.out.println("This is an anonymous class");
             System.out.println("Name: "+name);
             System.out.println("Age: "+age);
-        }   
+        } 
+        @Override
+        public void extraMethod(){
+            System.out.println("This is an extra method in anonymous class");
+        }  
+        @Override
+         public void printTown(){
+            System.out.println("This is a method in anonymous class");
+         }
     };
-    obj.display();
+    obj.display();// ✅ Now allowed
+    obj.extraMethod();// ✅ Now allowed
+    obj.printTown();// ✅ Now allowed
  }
 }
