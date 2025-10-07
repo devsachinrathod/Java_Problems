a332c431a9423f9be936ee56b146bdb6ce8de266
Sachin Rathod <sr5631080@gmail.com>
2025-10-07 23:45:03 +0530

    file data reading by bufferReader

--- FileHanding/FileInputOutput.java
+++ FileHanding/FileInputOutput.java
@@ -1,25 +1,19 @@
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 // import java.io.exceptions;
 
 class FileInputOutput {
 
     public static void main(String[] args) {
         System.out.println("File Input/Output operations will be demonstrated here.");
-        try {
-            FileOutputStream fout = new FileOutputStream("student-admission.txt");
-            String data = "this is the data";
-            fout.write(data.getBytes());
-            fout.close();
-            FileInputStream file = new FileInputStream("student-admission.txt");
-            int i;
-            while ((i = file.read()) != -1) {
-                System.out.print((char) i);
+        try (BufferedReader br = new BufferedReader(new FileReader("student-admission.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
             }
-            file.close();
         } catch (Exception e) {
-            System.out.println("Error handling file." + e);
+            System.out.println("Error handling file: " + e);
         }
     }
 }
