cb9ffb50ffe6ed3186eb8cc7f1f88e038a693321
Sachin Rathod <sr5631080@gmail.com>
2025-10-07 23:33:33 +0530

    File handling with inputOutputStream

--- FileHanding/FileInputOutput.java
+++ FileHanding/FileInputOutput.java
@@ -1,17 +1,25 @@
+
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 // import java.io.exceptions;
+
 class FileInputOutput {
+
     public static void main(String[] args) {
         System.out.println("File Input/Output operations will be demonstrated here.");
-
-       try {
-            FileInputStream  file = new FileInputStream("student-admission.txt");
+        try {
+            FileOutputStream fout = new FileOutputStream("student-admission.txt");
+            String data = "this is the data";
+            fout.write(data.getBytes());
+            fout.close();
+            FileInputStream file = new FileInputStream("student-admission.txt");
             int i;
             while ((i = file.read()) != -1) {
                 System.out.print((char) i);
             }
-       } catch (Exception e) {
-       }
-        // Add file handling code as needed
+            file.close();
+        } catch (Exception e) {
+            System.out.println("Error handling file." + e);
+        }
     }
 }
