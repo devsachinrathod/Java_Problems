f6c1e20c1320b880d84b57c8aa4a9f1118c29733
Sachin Rathod <sr5631080@gmail.com>
2025-10-08 00:08:57 +0530

    Data Strcture in java

--- Java-data-stucture/Data-struct.java
+++ Java-data-stucture/Data-struct.java
@@ -1,21 +1,37 @@
 
-import java.util.HashSet;
+import java.util.HashMap;
 
-class Ds{
+
+// import java.util.HashSet;
+
+// class Ds {
+
+//     public static void main(String[] args) {
+//         System.out.println(".()");
+//         HashSet<String> cars = new HashSet<String>();
+//         cars.add("Volvo");
+//         cars.add("BMW");
+//         cars.add("Ford");
+//         cars.add("Ford");
+//         cars.add("Ford");
+//         cars.add("Ford");
+//         cars.add("Ford");
+//         cars.add("Ford");
+//         cars.add("Ford");
+//         cars.add("BMW");  // Duplicate
+//         cars.add("Mazda");
+//         System.out.println(cars);
+//     }
+// }
+
+
+class ds{
     public static void main(String[] args) {
-        System.out.println(".()");
-HashSet<String> cars = new HashSet<String>();
-    cars.add("Volvo");
-    cars.add("BMW");
-    cars.add("Ford");
-        cars.add("Ford");
-               cars.add("Ford");
-                      cars.add("Ford");
-                             cars.add("Ford");
-                                    cars.add("Ford");
-                                           cars.add("Ford");
-    cars.add("BMW");  // Duplicate
-    cars.add("Mazda");
-    System.out.println(cars);
+        HashMap<String , String> emp = new HashMap<String , String>();
+        emp.put("sachin", "TYBCA");
+        emp.put("diapk", "SYBCA"); 
+
+        System.out.println("data"+ emp);
     }
+
 }
