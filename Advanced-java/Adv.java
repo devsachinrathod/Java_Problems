800c88056a4a77092b242e650a83fbadef017861
Sachin Rathod <sr5631080@gmail.com>
2025-10-23 19:40:03 +0530

    array practice

--- Advanced-java/Adv.java
+++ Advanced-java/Adv.java
@@ -1,20 +1,46 @@
+
 import java.util.*;
-interface HomeLone{
-   void arrayNumber();
+
+interface HomeLone {
+
+    void arrayNumber();
 }
-class Adv implements HomeLone{
-    @Override
-   public void arrayNumber(){
+abstract class AnimalDemo {
+
+    // abstract method (no body)
+    abstract void sound();
+
+}
+class Adv extends AnimalDemo implements HomeLone {
+
+    void sound() {
+        System.out.println("Animal sound");
+    }
     ArrayList<Integer> arr = new ArrayList<>();
 
-    System.err.println("Here show array number");
-   }
-    
-      void InfoDisplay(){
-     arr.add("sachin");
-     
+    public void arrayNumber() {
+        arr.add(12);
+        System.err.println("Here show array number");
     }
+    public void arrayNumber(int a) {
+        arr.add(a);
+        System.err.println("Here show array number with parameter: " + a);
+    }
+
+    void addData() { // ✅ defined outside arrayNumber()
+        arr.add(100);
+        System.out.println("Data added: " + arr);
+    }
+
     public static void main(String[] args) {
+        Adv A1 = new Adv();
+        A1.arrayNumber();
+          A1.arrayNumber(5);
+        A1.addData();
+      
+        A1.sound();
+     
+
         Integer myInt = 100;
         String myString = myInt.toString();
         System.out.println(myString.length());
