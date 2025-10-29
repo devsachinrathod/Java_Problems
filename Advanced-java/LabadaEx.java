324bf470ff70beeeeb20fc7628a101fe55878140
Sachin Rathod <sr5631080@gmail.com>
2025-10-29 22:53:56 +0530

    Labada

---
+++ Advanced-java/LabadaEx.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.util.function.BiConsumer;
+
+class LabadaEx {
+
+
+    public static void main(String[] args) {
+        // Lambda expression to implement a functional interface
+        Runnable r = () -> System.out.println("Hello from Lambda Expression!");
+        
+        BiConsumer<Integer, Integer> p = (a, b) -> {
+            System.out.println("Sum: " + (a + b));
+        };
+        // Running the lambda expressions
+        r.run();
+        p.accept(5, 10);
+            ArrayList<Integer> numbers = new ArrayList<Integer>();
+    numbers.add(5);
+    numbers.add(9);
+    numbers.add(8);
+    numbers.add(1);
+    numbers.forEach((n) -> { System.out.println(n); });
+    }
+}
--- Advanced-java/javaThreed.java
+++ Advanced-java/javaThreed.java
@@ -1,21 +1,34 @@
-
-class additionalClass {
-
+class additionalClass extends Thread {
+ 
+  public void run() {
+        System.out.println("top 1 " + Thread.currentThread().getName());
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Value of i in additional class: " + i);
+            try {
+                Thread.sleep(400); // Pause for 400 milliseconds
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted: " + e);
+            }
+        }
+    }
     void display() {
-        System.out.println("This is an additional class inside javaThreed");
+        System.out.println("This is an additional class inside javaThreed " + Thread.currentThread().getName());
         for (int j = 1; j <= 5; j++) {
             System.out.println("Value of j in additional class: " + j);
         }
     }
+ 
 
 }
 
 class javaThreed extends Thread {
-
+   javaThreed() {
+        super("MyThread"); // Set thread name
+    }
     public void run() {
-        System.out.println("This is a thread class");
+        System.out.println("Top 2  " + Thread.currentThread().getName());
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Thread running: " + i);
+            System.out.println("all about thread : " + i);
             try {
                 Thread.sleep(500); // Pause for 500 milliseconds
             } catch (InterruptedException e) {
@@ -27,9 +40,10 @@ class javaThreed extends Thread {
     public static void main(String[] args) {
         javaThreed t1 = new javaThreed();
 
-        t1.start();
+        t1.start(); // starts a new thread
+
         additionalClass a1 = new additionalClass();
-        System.out.println("This is a 3D class");
-        a1.display();
+        System.out.println("This is a 3D class " + Thread.currentThread().getName());
+        a1.display(); // runs in the main thread
     }
 }
