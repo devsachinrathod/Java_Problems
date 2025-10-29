061021eeeb332e650a2612c105659ccafa31e4dc
Sachin Rathod <sr5631080@gmail.com>
2025-10-29 23:36:43 +0530

    Labada

--- Advanced-java/LabadaEx.java
+++ Advanced-java/LabadaEx.java
@@ -3,9 +3,31 @@ import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
+interface MyInterface {
+    String sayHello(String name);
+}
+
+interface MyExper {
+    String exper();
+}
+
 public class LabadaEx {
 
     public static void main(String[] args) {
+
+        MyExper s = () -> {
+            return "Hello, this is a lambda expression!";
+        };
+        System.out.println(s.exper());
+        System.out.println(s.exper());
+
+        MyInterface myInterface = (name) -> {
+            return "Hello, " + name + "!";
+        };
+
+        formatString("sachin", myInterface);
+
+        System.out.println(myInterface.sayHello("Ravi"));
         // Lambda expression to implement a functional interface
         Runnable r = () -> System.out.println("Hello from Lambda Expression!");
 
@@ -20,8 +42,15 @@ public class LabadaEx {
         numbers.add(9);
         numbers.add(8);
         numbers.add(1);
-        Consumer<Integer> nums = (n) -> {System.out.println("Output"+ n);};
+        Consumer<Integer> nums = (n) -> {
+            System.out.println("Output" + n);
+        };
         numbers.forEach(nums);
     }
+ 
+
+    public static void formatString(String str, MyInterface cons) {
+        String result = cons.sayHello(str);
+        System.out.println(result);
+    }
 }
-    
