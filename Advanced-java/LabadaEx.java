bdb7e147a85632c7648e5a751c92bd765eba8bcb
Sachin Rathod <sr5631080@gmail.com>
2025-10-29 23:03:58 +0530

    Labada

--- Advanced-java/LabadaEx.java
+++ Advanced-java/LabadaEx.java
@@ -1,25 +1,27 @@
 
 import java.util.*;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
-class LabadaEx {
-
+public class LabadaEx {
 
     public static void main(String[] args) {
         // Lambda expression to implement a functional interface
         Runnable r = () -> System.out.println("Hello from Lambda Expression!");
-        
+
         BiConsumer<Integer, Integer> p = (a, b) -> {
             System.out.println("Sum: " + (a + b));
         };
         // Running the lambda expressions
         r.run();
         p.accept(5, 10);
-            ArrayList<Integer> numbers = new ArrayList<Integer>();
-    numbers.add(5);
-    numbers.add(9);
-    numbers.add(8);
-    numbers.add(1);
-    numbers.forEach((n) -> { System.out.println(n); });
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(5);
+        numbers.add(9);
+        numbers.add(8);
+        numbers.add(1);
+        Consumer<Integer> nums = (n) -> {System.out.println("Output"+ n);};
+        numbers.forEach(nums);
     }
 }
+    
