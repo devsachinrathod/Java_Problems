e1591e4556b056a7c7c9dbf7ccb44aad26813926
Sachin Rathod <sr5631080@gmail.com>
2025-11-08 23:11:37 +0530

    add

--- Advanced-java/LabadaEx.java
+++ Advanced-java/LabadaEx.java
@@ -1,6 +1,3 @@
-
-import java.util.*;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 interface MyInterface {
@@ -9,48 +6,44 @@ interface MyInterface {
 
 interface MyExper {
     String exper();
+    Integer labaExpress();
 }
 
 public class LabadaEx {
 
     public static void main(String[] args) {
 
-        MyExper s = () -> {
-            return "Hello, this is a lambda expression!";
+        MyExper s = new MyExper() {
+            public String exper() {
+                return "Hello, this is a lambda expression!";
+            }
+
+            public Integer labaExpress() {
+                return 42;
+            }
         };
+
+        // OR using lambda expressions separately:
+        MyExper sLambda = new MyExper() {
+            public String exper() {
+                return "Hello, this is a lambda expression!";
+            }
+
+            public Integer labaExpress() {
+                return 42;
+            }
+        };
+
         System.out.println(s.exper());
-        System.out.println(s.exper());
-
-        MyInterface myInterface = (name) -> {
-            return "Hello, " + name + "!";
-        };
-
-        formatString("sachin", myInterface);
-
-        System.out.println(myInterface.sayHello("Ravi"));
-        // Lambda expression to implement a functional interface
-        Runnable r = () -> System.out.println("Hello from Lambda Expression!");
-
-        BiConsumer<Integer, Integer> p = (a, b) -> {
-            System.out.println("Sum: " + (a + b));
-        };
-        // Running the lambda expressions
-        r.run();
-        p.accept(5, 10);
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
-        numbers.add(5);
-        numbers.add(9);
-        numbers.add(8);
-        numbers.add(1);
-        Consumer<Integer> nums = (n) -> {
-            System.out.println("Output" + n);
-        };
-        numbers.forEach(nums);
-    }
+        System.out.println(s.labaExpress());
  
 
-    public static void formatString(String str, MyInterface cons) {
-        String result = cons.sayHello(str);
-        System.out.println(result);
+    integer value ->{
+        system.out.println("The integer value is: " + value);
+    }
+        Consumer<String> c = (name) -> {
+            System.out.println("Hello, " + name + "!");
+        };
+        c.accept("Diapk");
     }
 }
