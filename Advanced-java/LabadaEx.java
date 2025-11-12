e5412580b28326575bd5213e0adedfa91da49c17
Sachin Rathod <sr5631080@gmail.com>
2025-11-12 21:48:33 +0530

    Added the labada code

--- Advanced-java/LabadaEx.java
+++ Advanced-java/LabadaEx.java
@@ -1,49 +1,81 @@
-import java.util.function.Consumer;
+// import java.util.function.Consumer;
 
-interface MyInterface {
-    String sayHello(String name);
-}
+// interface MyInterface {
+//     String sayHello(String name);
+// }
 
-interface MyExper {
-    String exper();
-    Integer labaExpress();
-}
+// interface MyExper {
+//     String exper();
+//     Integer labaExpress();
+// }
+// interface MyLambda {
+//     Integer labaExpress();
+// }
+// abstract class ExampleLaba {
 
-public class LabadaEx {
+//     abstract void show();
+// }
 
-    public static void main(String[] args) {
+// public class LabadaEx extends ExampleLaba  {
 
-        MyExper s = new MyExper() {
-            public String exper() {
-                return "Hello, this is a lambda expression!";
-            }
+//     public static void main(String[] args) {
+//         MyInterface show = (name) -> "Hello, " + name;
 
-            public Integer labaExpress() {
-                return 42;
-            }
-        };
+//         MyLambda lambda = () -> 42;
+//         System.out.println("Lambda result: " + lambda.labaExpress());
 
-        // OR using lambda expressions separately:
-        MyExper sLambda = new MyExper() {
-            public String exper() {
-                return "Hello, this is a lambda expression!";
-            }
+//         MyExper s = new MyExper() {
+//             // @Override
+//             public String exper() {
+//                 return "Hello, this is a lambda expression!";
+//             }
 
-            public Integer labaExpress() {
-                return 42;
-            }
-        };
+//             public Integer labaExpress() {
+//                 return 42;
+//             }
+//         };
 
-        System.out.println(s.exper());
-        System.out.println(s.labaExpress());
+//         // OR using lambda expressions separately:
+//         MyExper sLambda = new MyExper() {
+//             public String exper() {
+//                 return "Hello, this is a lambda expression!";
+//             }
+
+//             public Integer labaExpress() {
+//                 return 42;
+//             }
+//         };
+
+//         System.out.println(s.exper());
+//         System.out.println(s.labaExpress());
  
 
-    integer value ->{
-        system.out.println("The integer value is: " + value);
+//         Consumer<String> c = (name) -> {
+//             System.out.println("Hello, " + name + "!");
+//         };
+//         c.accept("Diapk");
+//     }
+// }
+@FunctionalInterface
+interface UnderAgeWarning {
+    void showMessage();
+}
+
+class LabadaEx implements UnderAgeWarning {
+
+    // @Override
+    public void showMessage() {
+        System.out.println("You are underage! (from class)");
     }
-        Consumer<String> c = (name) -> {
-            System.out.println("Hello, " + name + "!");
-        };
-        c.accept("Diapk");
+
+    public static void main(String[] args) {
+        // Lambda implementation
+        UnderAgeWarning ex = () -> System.out.println("You are underage! (from lambda)");
+
+        ex.showMessage(); // Lambda output
+
+        // Class instance
+        LabadaEx instance = new LabadaEx();
+        instance.showMessage();
     }
 }
